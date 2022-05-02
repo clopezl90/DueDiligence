8249d065257a855a2a9668b465893660c6418ac4
Cristian López <ch.lopez@uniandes.edu.co>
2022-05-02 06:29:03 -0500

    jobs objects added for new panels. UI updated

---
+++ Assets/Scripts/JobOrders.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JobOrders
+{
+    public List<Orders> ordersList = new List<Orders>();
+
+    public JobOrders() { }
+}
--- Assets/Scripts/Jobs.cs
+++ Assets/Scripts/Jobs.cs
@@ -12,7 +12,11 @@ public class Jobs
     public string jobClient;
     public int jobReward;
     public string jobStatus;
+    public string jobTag;
     public Clients jobCientObject;
+    public Orders jobOrdersObject;
+    public JobEstimate jobEstimateObject;
+    public JobPayments jobPaymentsObject;
 
 
     public Jobs (string _jobDescription, string _jobClient, int _jobReward, string _jobStatus) 
