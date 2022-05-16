5e9460335ae2b1e616059cab60a1421a11081abb
Cristian <ch.lopez@uniandes.edu.co>
2022-05-16 15:04:47 -0500

    settings panel update

--- Assets/Scripts/SettingsMenuHandler.cs
+++ Assets/Scripts/SettingsMenuHandler.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsMenuHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Text companyName;
+    [SerializeField] Text companySite;
+    [SerializeField] Text companyPhone;
+    [SerializeField] Text companyEmail;
+    [SerializeField] Text tagText;
+    public GameObject tagContainer;
+    public Transform tagTramsform;
+    [SerializeField] Text customerTagText;
+    public GameObject customerTagContainer;
+    public Transform CustomerTagTramsform; 
     void Start()
     {
-        
+        AssingSettingValues();
+               
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
+    public void AssingSettingValues()
+    {
+        companyName.text = UserData.userRecords.companyName;
+        companySite.text = UserData.userRecords.companySite;
+        companyPhone.text = UserData.userRecords.companyphone;
+        companyEmail.text = UserData.userRecords.companyEmail; 
+        UpdateTags();
+
+    }
+    public void UpdateTags()
+    {
+        CleanTags();
+        foreach (string tag in UserData.userRecords.jobTags)
+        {
+            tagText.text = tag;            
+            GameObject _tempGo = Instantiate(tagContainer, tagTramsform);
+        }
+        foreach (string tag in UserData.userRecords.customerTags)
+        {
+            customerTagText.text = tag;            
+            GameObject _tempGo = Instantiate(customerTagContainer, CustomerTagTramsform);
+        }
+    }
+    public void CleanTags()
+    {
+        foreach (Transform child in tagTramsform.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+        foreach (Transform child in CustomerTagTramsform.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }        
+    }
+
 }
--- Assets/Scripts/UserRecords.cs
+++ Assets/Scripts/UserRecords.cs
@@ -10,8 +10,32 @@ public class UserRecords
 {
     public bool tutorial;
     public bool appPurchased;
+    public string companyName = "EasyTest";
+    public string companySite = "New York";
+    public string companyEmail = "easy@easy.co";
+    public string companyphone = "321654987";
+    public string companyWebsite = "www.easycoding.co";
+    public string measurementUnitInUse = "meters";
+    public string [] measurementUnits;
+    public string currencyInUse;
+    public string [] currency;
+    public string taxGroupInUse;
+    public string [] taxGroups;
+    public List <string> jobTags = new List<string>();
+    public List <string> customerTags = new List<string>();
     
+    public UserRecords ()
+    {
+        jobTags = new List<string>();
+        jobTags.Add("Commercial");
+        jobTags.Add("New Construction");
+        jobTags.Add("Renovation");
+        jobTags.Add("Residential");
+        jobTags.Add("Service");
 
-    public UserRecords (){}
+        customerTags = new List<string>();
+        customerTags.Add("High Value");
+        customerTags.Add("Property value");
+    }
     
 }
