87e322f83174644ddfd2e440834edc2ae9eaa4c4
Cristian <ch.lopez@uniandes.edu.co>
2022-05-18 17:32:10 -0500

    Cristian lo chupa cagado

--- Assets/Scripts/SettingsMenuHandler.cs
+++ Assets/Scripts/SettingsMenuHandler.cs
@@ -5,6 +5,11 @@ using UnityEngine.UI;
 
 public class SettingsMenuHandler : MonoBehaviour
 {
+    [SerializeField] GameObject noCompanyInfoText;
+    [SerializeField] GameObject companyInfo;
+    [SerializeField] InputField companyNameInputField;
+    [SerializeField] InputField companySiteInputField;
+    [SerializeField] InputField companyPhoneInputField;
     [SerializeField] Text companyName;
     [SerializeField] Text companySite;
     [SerializeField] Text companyPhone;
@@ -14,37 +19,52 @@ public class SettingsMenuHandler : MonoBehaviour
     public Transform tagTramsform;
     [SerializeField] Text customerTagText;
     public GameObject customerTagContainer;
-    public Transform CustomerTagTramsform; 
+    public Transform CustomerTagTramsform;
     void Start()
     {
         AssingSettingValues();
-               
+
     }
 
     void Update()
     {
-        
+
     }
     public void AssingSettingValues()
     {
-        companyName.text = UserData.userRecords.companyName;
-        companySite.text = UserData.userRecords.companySite;
-        companyPhone.text = UserData.userRecords.companyphone;
-        companyEmail.text = UserData.userRecords.companyEmail; 
-        UpdateTags();
+        if (UserData.userRecords.companyName == "")
+        {
+            companyInfo.SetActive(false);
+            noCompanyInfoText.SetActive(true);
+        }
+        else
+        {
+            companyName.text = UserData.userRecords.companyName;
+            companySite.text = UserData.userRecords.companySite;
+            companyPhone.text = UserData.userRecords.companyphone;
+            companyEmail.text = UserData.userValidationInfo.email;            
+            UpdateTags();
+            companyInfo.SetActive(true);
+        }
+    }
 
+    public void AssingCompanyInfo()
+    {
+        UserData.userRecords.companyName = companyNameInputField.text;
+        UserData.userRecords.companySite = companySiteInputField.text;
+        UserData.userRecords.companyphone = companyPhoneInputField.text;
     }
     public void UpdateTags()
     {
         CleanTags();
         foreach (string tag in UserData.userRecords.jobTags)
         {
-            tagText.text = tag;            
+            tagText.text = tag;
             GameObject _tempGo = Instantiate(tagContainer, tagTramsform);
         }
         foreach (string tag in UserData.userRecords.customerTags)
         {
-            customerTagText.text = tag;            
+            customerTagText.text = tag;
             GameObject _tempGo = Instantiate(customerTagContainer, CustomerTagTramsform);
         }
     }
@@ -57,7 +77,7 @@ public class SettingsMenuHandler : MonoBehaviour
         foreach (Transform child in CustomerTagTramsform.transform)
         {
             GameObject.Destroy(child.gameObject);
-        }        
+        }
     }
 
 }
