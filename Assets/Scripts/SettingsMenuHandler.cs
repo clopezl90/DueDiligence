c50a299f66eb210b48e0803486289599b0a2e307
Cristian <ch.lopez@uniandes.edu.co>
2022-06-03 17:32:41 -0500

    single family and itmes first configs

--- Assets/Scripts/SettingsMenuHandler.cs
+++ Assets/Scripts/SettingsMenuHandler.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SettingsMenuHandler : MonoBehaviour
 {
@@ -78,6 +79,10 @@ public class SettingsMenuHandler : MonoBehaviour
         {
             GameObject.Destroy(child.gameObject);
         }
+    }
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
     }
 
 }
